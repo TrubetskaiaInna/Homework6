56e204b65d5022b54ffa0570e27487819e9fd039
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-14 13:18:38 +0200

    added addMoneyAtm

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ class Atm {
   get showBalance () {
     return this.balance
   }
+
+  set addMoneyAtm (value) {
+    this.balance = value
+  }
 }
+
+const arr = [50, 100, 200, 300, 500, 1000]
+let someSumMoneyAtm = 1000   //input
+let someArrBillsAtm = [500, 200, 200, 100] //input
+let sumArrCup = 0
+let moneyAtm = []
+const addBillsAtm = (sum, arrBills) => {
+  let resultAddBillsAtm = 'Error!'
+  for (let i = 0; i < arrBills.length; i++) {
+    sumArrCup += arrBills[i]
+    if (sum === sumArrCup) {
+      moneyAtm.push(sum, arrBills)
+      resultAddBillsAtm = moneyAtm
+    }
+  }
+  return resultAddBillsAtm
+}
+
+addBillsAtm(someSumMoneyAtm,someArrBillsAtm)
 
 let atm = new Atm()
 
-console.log(atm.balance)
+atm.addMoneyAtm = moneyAtm
+console.log(atm.balance)  //button

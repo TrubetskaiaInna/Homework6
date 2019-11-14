b085cd272011cc751f3d2fe07ca0482ea9a80183
Trubetskaia Inna <trubetskaya_inna@ukr.net>
2019-11-14 23:49:14 +0200

    added validationBillsAvailability

--- index.js
+++ index.js
@@ -29,11 +29,32 @@ class Atm {
   }
 
   validationMoneyAtm = (money) => {
-    if (atm.balance.sum >= money) {
+    if (this.balance.sum >= money) {
       return true
     } else {alert('Error!Not enough money on the atm')}
   }
 
+  validationBillsAvailability = (money) => {
+    let rest
+    let issueBills = []
+    this.balance.bills.sort((a, b) => b - a)
+    let minBill = Math.min.apply(null, this.balance.bills)
+    if (money < minBill) {
+      alert(`Error! There is no ${money} in ATM choose another sum`)
+    } else {
+      for (let i = 0; i < this.balance.bills.length; i++) {
+        if (money >= this.balance.bills[i]) {
+          rest = money - this.balance.bills[i]
+          money = rest
+          issueBills.push(this.balance.bills[i])
+        }
+      }
+    }
+    if (rest !== 0) {
+      alert(`Error! There is no ${money} in ATM choose another sum`)
+    }
+    return issueBills
+  }
 }
 
 class CreditCard {
@@ -50,7 +71,7 @@ class CreditCard {
   }
 
   validationMoneyCard = (money) => {
-    if (creditCard.balance >= money) {
+    if (this.balance >= money) {
       return true
     } else {alert('Error!Not enough money on the card')}
   }
@@ -58,6 +79,7 @@ class CreditCard {
 
 const atm = new Atm()
 const creditCard = new CreditCard()
+const creditCard2 = new CreditCard()
 
 atm.addMoneyAtm(500, [200, 200, 100])
 atm.addMoneyAtm(1000, [500, 200, 200, 100])
@@ -66,6 +88,9 @@ creditCard.addMoneyCard(2000)
 creditCard.addMoneyCard(2000)
 creditCard.addMoneyCard(1000)
 console.log(atm.showBalance)
-console.log(creditCard.showBalance)
+console.log(1,creditCard.showBalance)
 creditCard.validationMoneyCard(sumMoneyGetCash)
 atm.validationMoneyAtm(sumMoneyGetCash)
+console.log(atm.validationBillsAvailability(sumMoneyGetCash))
+creditCard2.addMoneyCard(7000)
+console.log(2,creditCard2.showBalance)
